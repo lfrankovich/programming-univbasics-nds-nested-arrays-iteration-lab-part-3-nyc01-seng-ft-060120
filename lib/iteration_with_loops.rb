bdb57358f8a0d07e00aeac6f04e3fb7fdda809fe
Lauren Frankovich <frankovister@gmail.com>
2020-05-26 04:03:40 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ while row_index < src.count do
    while element_index < src[row_index].count do
       if src[row_index][element_index].class == String
       new_string.push(src[row_index][element_index])
-    end
+      end
       element_index += 1
    end
    row_index += 1
